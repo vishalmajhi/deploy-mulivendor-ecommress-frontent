ea97a29e451aed0a06f8681b16012eccecc9f107
vishal majhi <vishalmajhi33@gmail.com>
2024-01-11 13:52:00 +0530

    all testing completed

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Carousel } from "react-responsive-carousel";
-import { BiArrowBack } from "react-icons/bi";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const HeroBanner = () => {
@@ -9,7 +7,7 @@ const HeroBanner = () => {
       <div className="absolute top-0 left-0 w-full h-full md:h-[32rem] lg:h-[36rem] xl:h-[40rem] overflow-hidden rounded-xl">
        
           <img
-            src="../public/banner.jpg.jpg" // Update the image path
+            src="../public/banner.jpg.jpg" 
             alt="Image 2"
             className="w-full h-full object-cover"
           />
